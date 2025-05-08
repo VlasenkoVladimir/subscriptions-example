93b73583ac186ce088bd953966f253a4a35a6771
Vladimir Vlasenko <v.g.vlasenko@yandex.ru>
2025-05-08 19:34:13 +0300

    Init

---
+++ src/main/java/com/vlasenko/subscriptions_example/SubscriptionsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.vlasenko.subscriptions_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionsExampleApplication.class, args);
+	}
+
+}
